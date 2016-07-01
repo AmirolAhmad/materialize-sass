f1c210f06e6f2416bce8e46e9b67c8fbb9fca935
Amirol Ahmad <9w2pdf@gmail.com>
2016-07-02 06:31:43 +0800

    remove compass,lotus & sprockets

--- lib/materialize-sass.rb
+++ lib/materialize-sass.rb
@@ -3,16 +3,9 @@ module Materialize
   class << self
     # Inspired by Kaminari
     def load!
-      register_compass_extension if compass?
-
       if rails?
         register_rails_engine
-      elsif lotus?
-        register_lotus
-      elsif sprockets?
-        register_sprockets
       end
-
       configure_sass
     end
 
@@ -37,21 +30,8 @@ module Materialize
       @assets_path ||= File.join gem_path, 'assets'
     end
 
-    # Environment detection helpers
-    def sprockets?
-      defined?(::Sprockets)
-    end
-
-    def compass?
-      defined?(::Compass::Frameworks)
-    end
-
     def rails?
       defined?(::Rails)
-    end
-
-    def lotus?
-      defined?(::Lotus)
     end
 
     private
@@ -63,28 +43,8 @@ module Materialize
       ::Sass::Script::Number.precision = [8, ::Sass::Script::Number.precision].max
     end
 
-    def register_compass_extension
-      ::Compass::Frameworks.register(
-          'materialize',
-          :version               => Materialize::VERSION,
-          :path                  => gem_path,
-          :stylesheets_directory => stylesheets_path,
-          :templates_directory   => File.join(gem_path, 'templates')
-      )
-    end
-
     def register_rails_engine
       require 'materialize-sass/engine'
-    end
-
-    def register_lotus
-      Lotus::Assets.sources << assets_path
-    end
-
-    def register_sprockets
-      Sprockets.append_path(stylesheets_path)
-      Sprockets.append_path(fonts_path)
-      Sprockets.append_path(javascripts_path)
     end
   end
 end
